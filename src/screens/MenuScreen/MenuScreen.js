fa420f2ace112afd4f060ec6cb159ef634e43501
Mahedimm <mahedi15-12649@diu.edu.bd>
2022-02-20 22:21:34 +0600

    menu

--- src/screens/MenuScreen/MenuScreen.js
+++ src/screens/MenuScreen/MenuScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import menuList from '../../../public/menuList.json';
+import Header from '../../components/Header/Header';
+import MenuItem from '../../components/MenuItem/MenuItem';
+import './MenuScreen.css';
 const MenuScreen = () => {
     return (
-        <div>
-            <h1>This Is menu</h1>
-        </div>
+       <>
+           <Header menuPage/>
+           <div className="menuScreen__container">
+               <div className="menuScreen__left">
+                   {/* <Menulist/> */}
+               </div>
+               <div className="menuScreen__right">
+                    <h1>Menu</h1>
+                    <div className="menuScreen__category">
+                        <h2>Drinks</h2>
+                        <div className="menuScreen__items">
+                            {menuList.map(menuListCategory => (menuListCategory.food.map(({type,path,image}) => (
+                                <MenuItem />
+                            ))))}
+                        </div>
+                    </div>
+               </div>
+           </div>
+       </>
     );
 };
 
